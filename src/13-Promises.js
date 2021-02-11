48a282c0da2dbc64f38d3393f523a5407b5c595c
vivianachoque <vivianaachoque@gmail.com>
2021-02-11 12:09:07 -0300

    repaso de promesas javascript

---
+++ src/13-Promises.js
@@ -0,0 +1,10 @@
+const promesa  = new Promise( (resolve, reject) =>{
+    setTimeout( () => {
+        resolve()
+    }, 2000)
+
+});
+
+promesa.then ( () => {
+console.log ('Then de la promesa')
+})
